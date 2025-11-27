be916f7b69f1a1b0eb5b4cc6fb5a4d6d322e426d
daniel_pisarev <danik.antyushev@yandex.ru>
2025-11-27 16:46:09 +0500

    finished(for git)

--- best_project/settings.py
+++ best_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a(oz=$%2!9%bj*h40nt468tmu)0cyw&qldwtlsb_$+=z47wyw7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # ALLOWED_HOSTS = []
